463f78ae9a10c2fdf6988a6be9379f1901e5a83c
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-12-27 12:10:26 +0500

    Add location

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,10 @@ import { Company } from "./Company"
 const user = new User()
 const company = new Company()
 
-console.log(user)
-console.log(company)
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: user.location.lat,
+    lng: user.location.lng,
+  },
+})
