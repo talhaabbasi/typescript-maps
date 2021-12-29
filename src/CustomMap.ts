4d03b14d206da7635d77f710a005736c699c537c
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-12-29 11:06:49 +0500

    Add implements clause

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,8 +1,10 @@
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number
     lng: number
   }
+  markerContent(): string
+  color: string
 }
 export class CustomMap {
   private googleMap: google.maps.Map
@@ -18,12 +20,18 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    })
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      })
+      infoWindow.open(this.googleMap, marker)
     })
   }
 }
