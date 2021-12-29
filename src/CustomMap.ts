a921436584e5847f8ff7ca9539be7980ce022847
Talha Abbasi <talhaabbasi1997@gmail.com>
2021-12-29 09:51:40 +0500

    Add addMarker function

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,5 @@
+import { User } from "./User"
+import { Company } from "./Company"
 export class CustomMap {
   private googleMap: google.maps.Map
 
@@ -7,6 +9,16 @@ export class CustomMap {
       center: {
         lat: 0,
         lng: 0,
+      },
+    })
+  }
+
+  addMarker(type: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: type.location.lat,
+        lng: type.location.lng,
       },
     })
   }
